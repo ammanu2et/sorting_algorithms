c61d99a27933a729ecc2b8805f1e78cf1154ec71
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-07 17:37:14 +0300

    Sorting Algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,24 +12,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t n;
-	int sorted;
+	int i, j, tmp;
 
 	if (!array || size == 0)
 		return;
 
-	do {
-    		sorted = 1;
-    		for (n = 0; n < size - 1; n++)
-    		{
-    			if (array[n] > array[n + 1])
-    			{
-    				sorted = 0;
-    				int tmp = array[n];
-    				array[n] = array[n + 1];
-    				array[n + 1] = tmp;
-    				print_array(array, size);
-    			}
-    		}
-
-    } while (sorted == 0);
+	for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size);
+            }
+        }
+    }
 }
