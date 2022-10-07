662530cc3646add2b98cab6d3d73ae69ebb96816
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-07 13:06:20 +0300

    Sorting Algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,7 +7,23 @@
  * @size: number of elements in array.
  */
 
+swap ( int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 void bubble_sort(int *array, size_t size)
 {
-
+n = size_t size;
+int i, j;
+for (i = 0; i < n - 1; i++)
+{
+    for (j = 0; j < n - i - 1; j++)
+    {
+        if (array[j] > array[j + 1])
+            swap (&array[j], &array[j + 1]);
+    }
+}
 }
