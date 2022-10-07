08eabd48546ca741239ff14f160c47927e6f18c2
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-07 17:46:08 +0300

    Sorting Algorithms

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -19,8 +20,11 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/*print functions.*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
