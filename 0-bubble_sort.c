5579ab6b2e18bcbf810c1f38177f73b7d14704ac
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-07 14:21:53 +0300

    Sorting Algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,7 +8,7 @@
  * Return: void
  */
 
-swap ( int *xp, int *yp)
+void swap( int *xp, int *yp)
 {
     int tmp = *xp;
     *xp = *yp;
@@ -24,7 +24,7 @@ for (i = 0; i < n - 1; i++)
     for (j = 0; j < n - i - 1; j++)
     {
         if (array[j] > array[j + 1])
-            swap (&array[j], &array[j + 1]);
+            swap(&array[j], &array[j + 1]);
     }
 }
 }
