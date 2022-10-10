266f8d7ca7852bc2abd9fad4ef6e0731807c24e2
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-10 17:49:17 +0300

    selection Sorting Algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,20 +8,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i;
-    int tmp, sorted;
+    size_t i, sorted, min_idx;
+    int tmp;
 
     for (i = 0; i < size - 1; i++)
-        int min_idx = i;
+    {
+        min_idx = i;
         for (sorted = i + 1; sorted < size; sorted++)
         {
             if (array[sorted] < array[min_idx])
                 min_idx = sorted;
-
-            if (min_idx != i)
-                tmp = array[min_idx];
-                array[min_idx] = array[i];
-                array[i] = tmp;
-                print_array(array, size);
         }
+        if (min_idx != i)
+        {
+            tmp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = tmp;
+            print_array(array, size);
+        }
+    }
 }
