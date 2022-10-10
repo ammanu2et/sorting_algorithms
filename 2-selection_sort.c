f185bb18838a3349df40d1a886ea8c084bacf008
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-10 17:30:39 +0300

    Insertion Sorting Algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,10 +1,31 @@
 #include 'sort.h'
 
 /**
- *
+ * selection_sort - sort a given elements using selection sort algorithm.
+ * @size: size of element
+ * Return: void
  */
 
 void selection_sort(int *array, size_t size)
 {
+    size_t i;
+    int tmp, sorted;
 
+    do
+    {
+        for (i = 0; i < size - 1; i++)
+        int min_idx = i;
+            for (int sorted = i + 1; sorted < size; sorted++)
+            {
+                if (array[sorted] < array[min_idx])
+                    min_idx = sorted;
+            }
+            tmp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = tmp;
+            print_array(array, size);
+            }
+        }
+    }
+    while (sorted == 0);
 }
