6e425b0cc54c3c508eb9a3c74a6aacdc8cc1aafc
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-10 15:06:19 +0300

    Insertion Sorting Algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,44 +1,46 @@
-#include 'sort.h'
+#include "sort.h"
 
 /**
- * insertion_sort_list - sort a list or array by insertion method.
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algorithm
  *
  * @list: doubly linked list
  * Return: no return
  */
-
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *ptr, *tmp;
+	listint_t *ptr, *tmp;
 
-    if (!list)
-        return;
+	if (!list)
+		return;
 
-    ptr = *list;
-    while (ptr)
-    {
-        while (ptr->next && (ptr->n > ptr->next->n))
-        {
-            tmp = ptr->next;
-            ptr->next = tmp->next;
-            tmp->prev = ptr->prev;
+	ptr = *list;
 
-            if (ptr->prev)
-                ptr->prev->next = tmp;
+	while (ptr)
+	{
+		while (ptr->next && (ptr->n > ptr->next->n))
+		{
+			tmp = ptr->next;
+			ptr->next = tmp->next;
+			tmp->prev = ptr->prev;
 
-            if (tmp->next)
-                tmp->next->prev = ptr;
+			if (ptr->prev)
+				ptr->prev->next = tmp;
 
-            ptr->prev = tmp;
-            tmp->next = ptr;
+			if (tmp->next)
+				tmp->next->prev = ptr;
 
-            if (tmp->prev)
-                ptr = tmp->prev;
-            else
-                *list = tmp;
+			ptr->prev = tmp;
+			tmp->next = ptr;
 
-            print_list(*list);
-        }
-        ptr = ptr->next;
-    }
+			if (tmp->prev)
+				ptr = tmp->prev;
+			else
+				*list = tmp;
+
+			print_list(*list);
+		}
+		ptr = ptr->next;
+	}
 }
