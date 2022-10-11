f54d571a2ef2e9e67204e57cf3926c13fc85ce2e
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-11 15:27:42 +0300

    shell Sorting Algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,10 +1,28 @@
-#include 'sort.h'
+#include "sort.h"
 
 /**
- *
+ * shell_sort - sort the list and print the changes
+ * @array: The array to sort.
+ * @size: Size of the array
+ * Return: Nothing
  */
 
 void shell_sort(int *array, size_t size)
 {
+    size_t n;
+    int interval, tmp, i, j;
 
+    for (interval = n/2; interval > 0; interval /= 2)
+    {
+        for (i = interval; i < n; i += 1)
+        {
+            tmp = array[i];
+            for (j = i; j >= interval && array[j - interval] > tmp; j -= interval)
+            {
+                array[j] = array[j - interval];
+            }
+            array[j] = tmp;
+            print_array(array, size);
+        }
+    }
 }
