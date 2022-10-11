1dcfebd91bd9e0e86127049d2808ef54885a0bcb
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-11 15:30:40 +0300

    shell Sorting Algorithms

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,28 +1,46 @@
 #include "sort.h"
 
 /**
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * Return: Nothing
+ */
+void _swap(int *array, int i, int j)
+{
+	int tmp;
+
+	if (array[i] != array[j])
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+}
+
+/**
  * shell_sort - sort the list and print the changes
  * @array: The array to sort.
  * @size: Size of the array
  * Return: Nothing
  */
-
 void shell_sort(int *array, size_t size)
 {
-    size_t n;
-    int interval, tmp, i, j;
+	size_t h = 0,  i, j;
 
-    for (interval = n/2; interval > 0; interval /= 2)
-    {
-        for (i = interval; i < n; i += 1)
-        {
-            tmp = array[i];
-            for (j = i; j >= interval && array[j - interval] > tmp; j -= interval)
-            {
-                array[j] = array[j - interval];
-            }
-            array[j] = tmp;
-            print_array(array, size);
-        }
-    }
+	if (size < 2)
+		return;
+
+	while (h <= size / 3)
+		h = h * 3 + 1;
+
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+				_swap(array, j, j - h);
+		h /= 3;
+		print_array(array, size);
+	}
 }
