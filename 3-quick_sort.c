8cdaa67c90ba3179e2ad71afdc4c92c34803644e
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-10-11 11:07:49 +0300

    selection Sorting Algorithms

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,61 @@
 #include 'sort.h'
 
 /**
- *
+ * quick_sort_sub - sort a sub array by using quick sort algorithm
+ * @array: the sub array
+ * @low: the start of sub array
+ * @high: the end of sub array
+ * @size: the size of the sub array
  */
 
-void quick_sort(int *array, size_t size)
+void quick_sort_sub(int *array, size_t low, size_t high, size_t size)
+{
+    size_t i, j;
+    int tmp, pivot;
+
+    if ((low >= high) || (array == NULL))
+        return;
+
+    pivot = array[high];
+    i = low
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            if (i != j)
+            {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                print_array(array, size);
+            }
+            i++;
+        }
+    }
+    if (i != high)
+    {
+        tmp = array[i];
+        array[i] = array[high];
+        array[high] = tmp;
+        print_array(array, size);
+    }
+    if (i - low > 1)
+        quick_sort_sub(array, low, i - 1, size);
+    if (high - i > 1)
+        quick_sort_sub(array, i + 1, high, size);
+}
+
+/**
+ * quick_sort - sort a given element by using quick sort algorithm
+ * @array: an array to sort
+ * @size: the size of the array or element
+ * Return: void
+ */
+void quick_sort(int *array, size_t size);
+{
+    if (array != NULL)
+    {
+        quick_sort_sub(array, 0, size - 1, size);
+    }
+}
